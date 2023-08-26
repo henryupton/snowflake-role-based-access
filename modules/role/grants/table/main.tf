6f5c530a762b5d1430d63b761ac1a15f1fa7068e
henrybirdsall <henry.birdsall@trademe.co.nz>
2023-08-26 21:53:13 +1200

    Added support for multiple grants.

--- modules/role/grants/table/main.tf
+++ modules/role/grants/table/main.tf
@@ -35,6 +35,21 @@ locals {
       with_grant_option = v.with_grant_option
     } if v.name == "*"
   }
+
+  _futures_by_grant = flatten([
+    for k, v in local.futures : [
+      for g in v.grants : {
+        database = v.database
+        schema   = v.schema
+
+        grant             = g
+        with_grant_option = v.with_grant_option
+      }
+    ]
+  ])
+  futures_by_grant = {
+    for i in local._futures_by_grant : lower("${i.database}.${i.schema}|${i.grant}") => i
+  }
 }
 
 # Retrieve all tables in each of the provided schemas.
@@ -48,38 +63,40 @@ data "snowflake_tables" "table_wildcard" {
 locals {
   resolved_wildcards = flatten([
     for k, v in local.input : [
-      for t in coalesce(data.snowflake_tables.table_wildcard[k].tables, []) : {
-        database = upper(t.database)
-        schema   = upper(t.schema)
-        name     = upper(t.name)
+      for t in coalesce(data.snowflake_tables.table_wildcard[k].tables, []) : [
+        for g in v.grants : {
+          database = upper(t.database)
+          schema   = upper(t.schema)
+          name     = upper(t.name)
 
-        fqn = upper("${t.database}.${t.schema}.${t.name}")
+          fqn = upper("${t.database}.${t.schema}.${t.name}")
 
-        pattern_matched = k
+          pattern_matched = k
 
-        grants            = v.grants
-        with_grant_option = v.with_grant_option
-      }
-      if startswith(
-        lower(t.name),
-        lower(split("*", v.name)[0])  # Match tables which share the prefix before the wildcard.
-      )
+          grant             = g
+          with_grant_option = v.with_grant_option
+        }
+        if startswith(
+          lower(t.name),
+          lower(split("*", v.name)[0])  # Match tables which share the prefix before the wildcard.
+        )
+      ]
     ]
   ])
 
-  tables = {
-    for i in local.resolved_wildcards : lower("${i.database}.${i.schema}.${i.name}")=> i
+  tables_by_grant = {
+    for i in local.resolved_wildcards : lower("${i.database}.${i.schema}.${i.name}|${i.grant}")=> i
   }
 }
 
 resource "snowflake_table_grant" "grant" {
-  for_each = local.tables
+  for_each = local.tables_by_grant
 
   database_name = each.value.database
   schema_name   = each.value.schema
   table_name    = each.value.name
 
-  privilege = each.value.grants[0]
+  privilege = each.value.grant
   roles     = [upper(var.role_name)]
 
   with_grant_option      = each.value.with_grant_option
@@ -87,12 +104,12 @@ resource "snowflake_table_grant" "grant" {
 }
 
 resource "snowflake_table_grant" "futures" {
-  for_each = local.futures
+  for_each = local.futures_by_grant
 
   database_name = each.value.database
   schema_name   = each.value.schema
 
-  privilege = each.value.grants[0]
+  privilege = each.value.grant
   roles     = [upper(var.role_name)]
 
   on_future              = true
@@ -102,7 +119,8 @@ resource "snowflake_table_grant" "futures" {
 
 output "debug" {
   value = {
-    "00_input"              = local.input
-    "05_resolved_wildcards" = local.resolved_wildcards
+    "00_input"            = local.input
+    "04_futures_by_grant" = local.futures_by_grant
+    "05_tables_by_grant"  = local.tables_by_grant
   }
 }
