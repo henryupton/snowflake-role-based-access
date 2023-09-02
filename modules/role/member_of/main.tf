81d0e97931010d41d634b690426adfb465b8b396
henrybirdsall <henry.birdsall@trademe.co.nz>
2023-09-02 21:49:08 +1200

    Removed support for wildcards in role to role grants.
    This is due to the well known '"for_each" depends on resource attributes that cannot be determined until apply' property of Terraform.

--- modules/role/member_of/main.tf
+++ modules/role/member_of/main.tf
@@ -13,52 +13,52 @@ terraform {
 
 locals {
   include = {
-    for role_pattern in coalesce(var.include, []) : upper(replace(role_pattern, "*", "%")) => null
+    for role_pattern in coalesce(var.include, []) : lower(replace(role_pattern, "*", "%")) => null
   }
   exclude = {
-    for role_pattern in coalesce(var.exclude, []) : upper(replace(role_pattern, "*", "%")) => null
+    for role_pattern in coalesce(var.exclude, []) : lower(replace(role_pattern, "*", "%")) => null
   }
 }
 
-data "snowflake_roles" "include" {
-  for_each = local.include
-
-  pattern = each.key
-}
-
-data "snowflake_roles" "exclude" {
-  for_each = local.exclude
-
-  pattern = each.key
-}
-
-locals {
-  include_flattened = toset([
-    for response in data.snowflake_roles.include : [
-      for role in response.roles : role.name
-    ]
-  ])
-  exclude_flattened = toset([
-    for response in data.snowflake_roles.exclude : [
-      for role in response.roles : role.name
-    ]
-  ])
-
-  roles = {
-    for role in setsubtract(local.include_flattened, local.exclude_flattened) : role => null
-  }
-}
-
-#resource "snowflake_role_grants" "grants" {
-#  for_each = local.roles
+#data "snowflake_roles" "include" {
+#  for_each = local.include
 #
-#  role_name = each.key
+#  pattern = each.key
+#}
 #
-#  roles = [var.role_name]
+#data "snowflake_roles" "exclude" {
+#  for_each = local.exclude
 #
-#  provider = snowflake.securityadmin
+#  pattern = each.key
+#}
+#
+#locals {
+#  include_flattened = toset([
+#    for response in data.snowflake_roles.include : [
+#      for role in response.roles : role.name
+#    ]
+#  ])
+#  exclude_flattened = toset([
+#    for response in data.snowflake_roles.exclude : [
+#      for role in response.roles : role.name
+#    ]
+#  ])
+#
+#  roles = toset([
+#    for role in local.include_flattened : role if !contains(local.exclude_flattened, role)
+#  ])
 #}
 
-output "roles" {
-  value = [for role in local.roles : lower(role)]
+resource "snowflake_role_grants" "grants" {
+  for_each = local.include  # local.roles
+
+  role_name = upper(each.key)
+
+  roles = [upper(var.role_name)]
+
+  provider = snowflake.securityadmin
 }
+
+#output "roles" {
+#  value = [for role in local.roles : lower(role)]
+#}
