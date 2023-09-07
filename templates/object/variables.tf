407acd44bb1a251e6d9644f020a08ce989c5031c
henrybirdsall <henry.birdsall@trademe.co.nz>
2023-09-08 09:13:40 +1200

    Added template file for variables.

---
+++ templates/object/variables.tf
@@ -0,0 +1,10 @@
+variable "{{ object }}s" {
+  type = map(object({
+    grants            = list(string)
+    with_grant_option = optional(bool)
+  }))
+}
+
+variable "role_name" {
+  type = string
+}
