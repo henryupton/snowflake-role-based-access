3e3aa00961e7e36920ddfd0e49df4bb3425823f6
henrybirdsall <henry.birdsall@trademe.co.nz>
2023-09-24 13:43:34 +1300

    Renamed object level grant directory.

---
+++ modules/role/privileges/object/warehouse/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source                = "Snowflake-Labs/snowflake"
+      version               = "0.66.1"
+      configuration_aliases = [
+        snowflake,
+        snowflake.securityadmin,
+      ]
+    }
+  }
+}
+
+locals {
+  _objects_by_grant = flatten([
+    for k, v in var.warehouses : [
+      for g in v.grants : {
+        name = k
+
+        grant             = g
+        with_grant_option = v.with_grant_option
+      }
+    ]
+  ])
+
+  objects_by_grant = merge(
+    {
+      for i in local._objects_by_grant : lower("${i.name}|${i.grant}") => i
+    }
+  )
+}
+
+resource "snowflake_warehouse_grant" "grant" {
+  for_each = local.objects_by_grant
+
+  warehouse_name = upper(each.value.name)
+
+  privilege = upper(each.value.grant)
+  roles     = [upper(var.role_name)]
+
+  with_grant_option      = each.value.with_grant_option
+  enable_multiple_grants = true
+
+  provider = snowflake.securityadmin
+}
+
+output "return" {
+  value = [ for k, v in var.warehouses : k ]
+}
